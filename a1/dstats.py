b81d31c63487d2ca769ec66b317d19f4ca5650de
Ken Tjhia <KenTjhia@gmail.com>
2019-10-11 19:54:35 -0400

    refactored dstats.py, overwriting one df

--- a1/dstats.py
+++ a1/dstats.py
@@ -1,23 +1,28 @@
 import pandas as pd
 import argparse
 
+# expects name of file containing business info and a a city name to query
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help='file containing the businesses')
 parser.add_argument('city', help='the city to compute statistics for')
 args = parser.parse_args()
 
-data = pd.read_csv(args.filename, usecols=['city', 'review_count', 'categories', 'stars'])
+# read all of the relevant columns all at once. I do this because the file is small (~30MB),
+# the 'full' dataset is only required to briefly stay in memory.
+df = pd.read_csv(args.filename, usecols=['city', 'review_count', 'categories', 'stars'])
 # get DataFrame pertaining to the city
-city_data = data[data['city'] == args.city]
-numOfBus = len(city_data)
-avgStars = city_data['stars'].mean()
+df = df[df['city'] == args.city]
+# compute the quantities pertaining to ALL of the businesses now
+avgNumOfReviews = df['review_count'].mean()
+numOfBus = len(df)
+avgStars = df['stars'].mean()
 
-# get DataFrame pertaining to businesses in the city with 'restaurant' in category string
-city_restaurants = city_data[city_data['categories'].str.contains('restaurant', case=False)]
-numOfRestaurants = len(city_restaurants)
-avgStarsRestaurants = city_restaurants['stars'].mean()
-avgNumOfReviews = city_data['review_count'].mean()
-avgNumOfReviewsBus = city_restaurants['review_count'].mean()
+# slice df to contain only rowsof  businesses in the city with 'restaurant' in category string
+df = df[df['categories'].str.contains('restaurant', case=False)]
+# compute quantities pertaining to the restaurants within the city
+numOfRestaurants = len(df)
+avgStarsRestaurants = df['stars'].mean()
+avgNumOfReviewsBus = df['review_count'].mean()
 
 print('numOfBus: {}\n'
       'avgStars: {:.2f}\n'
@@ -25,5 +30,5 @@ print('numOfBus: {}\n'
       'avgStarsRestaurants: {:.2f}\n'
       'avgNumOfReviews: {:.2f}\n'
       'avgNumOfReviewsBus: {:.2f}'
-      .format(numOfBus, avgStars, numOfRestaurants, avgStarsRestaurants
-              , avgNumOfReviews, avgNumOfReviewsBus))
+      .format(numOfBus, avgStars, numOfRestaurants, avgStarsRestaurants,
+              avgNumOfReviews, avgNumOfReviewsBus))
